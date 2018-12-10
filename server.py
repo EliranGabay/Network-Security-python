a078b6cd955f03ea158fc8b910ad096b2f1695e3
elirangabay <eli00ran@gmail.com>
2018-12-10 22:26:14 +0200

    check save project

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#eliran gabay
 import socket
 UDP_PORT = 12321
 try:
@@ -6,12 +7,12 @@ try:
 except socket.error as error:
     print("Socket creation failed(error number:" + error.message + ")" + "\r\n")
 sock.bind(('', UDP_PORT))
-print("Socket binded to %s"% (UDP_PORT)+"\r\n")
+print("Socket binded to %s" % UDP_PORT+"\r\n")
 sock.listen(5)
 while True:
     client, addr = sock.accept()
     print("Got connection from", addr)
     client.send("You successfully connecting" + "\r\n")
-    data=client.recv(1024)
+    data = client.recv(1024)
     print(data, addr)
     client.close()
