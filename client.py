7cea91fac26fa7ab8de3ce97b1c7f04bd6e6dcff
elirangabay <eli00ran@gmail.com>
2018-12-15 17:39:18 +0200

    update client and server 17:39 15.12

--- client.py
+++ client.py
@@ -1,37 +1,46 @@
 #eliran gabay
-import socket
-import random
 import string
-import time
-import datetime
 
+from scapy.all import *
+class UDPacket:
+    def __init__(self,data,seqNum):
+        self.sizePacket = len(data.encode('utf-8'))
+        self.data = data
+        self.seq_num = seqNum
 #Split the data to Packets
 def SplitPack(data):
     # Get the size of data
     dataSize = len(data.encode('utf-8'))
     numOfPack = (dataSize/100)+1
     if dataSize>10:
-        data=[data[i:i+100] for i in range(0, dataSize, 100)]
-    return data
-
+        data=[data[n:n + 100] for n in range(0, dataSize, 100)]
+        udp_packets = []
+        for x in range(numOfPack):
+            udp_packets.append(UDPacket(data[x],x))
+    return udp_packets
 # Create and connect UDP network Client
 UDP_PORT = 12321
 UDP_LOCALHOST = '127.0.0.1'
 try:
-    UDPCSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    UDPCSock.bind((UDP_LOCALHOST, 8080))
+    UDPCSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    UDPCSock.bind((UDP_LOCALHOST, random.randint(1000,8888)))
 except socket.error as error:
     print error
-    # Send message to UDP server every 3 sec
-timeS = time.localtime().tm_sec
+    sys.exit()
+# Send message to UDP server every 3 sec
+text = "".join([random.choice(string.letters) for i in xrange(450)])
+text=SplitPack(text)
+try:
+    UDPCSock.connect((UDP_LOCALHOST,UDP_PORT))
+    print UDPCSock.recv(1024)
+except socket.error as error:
+    print error
+    sys.exit()
 while True:
-    text = "".join([random.choice(string.letters) for i in xrange(250)])
-    text=SplitPack(text)
-    if (timeS+3) == time.localtime().tm_sec:
-        for data in text:
-            UDPCSock.sendto(data, (UDP_LOCALHOST, UDP_PORT))
-            UDPCSock.sendto(str(text.index(data)), (UDP_LOCALHOST, UDP_PORT))
-        print datetime.datetime.now().time(), UDPCSock.recvfrom(1024)
-        timeS = time.localtime().tm_sec
-    if (timeS+3) >= 60:
-        timeS = time.localtime().tm_sec
+    for i in range(len(text)):
+        print text[i].data
+        UDPCSock.send(text[i].data.encode('utf-8'))
+        UDPCSock.send(str(text[i].seq_num).encode('utf-8'))
+    print datetime.now().time(), UDPCSock.recv(1024)
+    time.sleep(3)
+UDPCSock.close()
--- server.py
+++ server.py
@@ -1,18 +1,26 @@
 #eliran gabay
-import datetime
-import socket
-
+from scapy.all import *
 UDP_PORT = 12321
 UDP_LOCALHOST = "127.0.0.1"
 # Create and connect UDP network service
 try:
-    UDPSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    UDPSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     UDPSock.bind((UDP_LOCALHOST, UDP_PORT))
+    UDPSock.listen(10)
     print "UDP server successful run"
 except socket.error as error:
     print error
 # Get messages from clients
 while True:
-    data, client = UDPSock.recvfrom(1024)
-    print datetime.datetime.now().time(), " Message from:", client, ":", data
-    UDPSock.sendto("The massage received", client)
+    print "Waiting for client..."
+    client, addr=UDPSock.accept()
+    print client.getpeername()," connect to server"
+    client.send("Connecting server successful".encode('utf-8'))
+    while True:
+        data = client.recv(1024)
+        print data
+        if not data:
+            client.send("The massage received".encode('utf-8'))
+            break
+    #print datetime.now().time(), " Message from:", client, ":", data.decode('utf-8')
+    client.close()
