b436cc393b62a7e70b6b952343aac70753695407
elirangabay <eli00ran@gmail.com>
2018-12-16 23:52:23 +0200

    final project server and client UDP whit loss packet

--- client.py
+++ client.py
@@ -1,46 +1,51 @@
-#eliran gabay
+#eliran gabay 203062831
 import string
-
 from scapy.all import *
-class UDPacket:
-    def __init__(self,data,seqNum):
-        self.sizePacket = len(data.encode('utf-8'))
-        self.data = data
-        self.seq_num = seqNum
+UDP_PORT = 12321
+UDP_LOCALHOST = '127.0.0.1'
 #Split the data to Packets
 def SplitPack(data):
-    # Get the size of data
     dataSize = len(data.encode('utf-8'))
-    numOfPack = (dataSize/100)+1
-    if dataSize>10:
+    numOfPack = (dataSize/100)
+    if dataSize>100:
         data=[data[n:n + 100] for n in range(0, dataSize, 100)]
         udp_packets = []
         for x in range(numOfPack):
-            udp_packets.append(UDPacket(data[x],x))
-    return udp_packets
-# Create and connect UDP network Client
-UDP_PORT = 12321
-UDP_LOCALHOST = '127.0.0.1'
+            udp_packets.append(str(x)+"#"+str(numOfPack)+"#"+data[x])
+    return udp_packets,data
+# String Xor
+def strXor(s1,s2):
+    return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
+# Calculates e (Task_2)
+def Cal_e(data):
+    numOfPack = len(data)
+    d = random.randint(0,numOfPack-1)
+    e = ""
+    for x in range(numOfPack-1):
+        if e =="":
+            e = data[x]
+        else:
+            e = strXor(e,data[x+1])
+    return e, d
+#Create and connect UDP network Client
 try:
-    UDPCSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    UDPCSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     UDPCSock.bind((UDP_LOCALHOST, random.randint(1000,8888)))
 except socket.error as error:
     print error
     sys.exit()
-# Send message to UDP server every 3 sec
-text = "".join([random.choice(string.letters) for i in xrange(450)])
-text=SplitPack(text)
-try:
-    UDPCSock.connect((UDP_LOCALHOST,UDP_PORT))
-    print UDPCSock.recv(1024)
-except socket.error as error:
-    print error
-    sys.exit()
+#Send message to UDP server every 3 sec
+text = "".join([random.choice(string.letters) for i in xrange(400)])
+Pack,data = SplitPack(text)
+e, d = Cal_e(data)
 while True:
-    for i in range(len(text)):
-        print text[i].data
-        UDPCSock.send(text[i].data.encode('utf-8'))
-        UDPCSock.send(str(text[i].seq_num).encode('utf-8'))
-    print datetime.now().time(), UDPCSock.recv(1024)
+    UDPCSock.sendto(str(d),(UDP_LOCALHOST,UDP_PORT))
+    for i in range(len(Pack)):
+        if i is not d:
+            print "send:"+Pack[i]+"#"+str(e)
+            UDPCSock.sendto(str(Pack[i])+"#"+str(e),(UDP_LOCALHOST,UDP_PORT))
+            time.sleep(0.1)
+        else:
+            print Pack[i]
+    print UDPCSock.recv(1024)
     time.sleep(3)
-UDPCSock.close()
--- server.py
+++ server.py
@@ -1,26 +1,62 @@
-#eliran gabay
+#eliran gabay 203062831
 from scapy.all import *
 UDP_PORT = 12321
 UDP_LOCALHOST = "127.0.0.1"
+# String Xor
+def strXor(s1,s2):
+    return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
+# Calculates e (Task_2)
+def Cal_e(data):
+    numOfPack = len(data)
+    e = ""
+    for x in range(numOfPack-1):
+        if e =="":
+            e = data[x]
+        else:
+            e = strXor(e,data[x+1])
+    return e
+# Receive data from client, check and build the Packets
+def readPacketsBuild(d):
+    message=[]
+    count = 0
+    flag = True
+    while True:
+        Packets, addr = UDPSock.recvfrom(1024)
+        data = Packets.split('#')
+        seqPack = int(data[0])
+        totalPack = int(data[1])
+        message.append(data[2])
+        e = data[3]
+        if seqPack == count:
+            count+=1
+            if flag:
+                flag = True
+        else:
+            count = seqPack+1
+            flag = False
+        if totalPack == seqPack+1:
+            break
+    return message, addr, e, flag
+#Build full message
+def buildMessage(meg):
+    return ''.join(meg)
 # Create and connect UDP network service
 try:
-    UDPSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    UDPSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     UDPSock.bind((UDP_LOCALHOST, UDP_PORT))
-    UDPSock.listen(10)
     print "UDP server successful run"
 except socket.error as error:
     print error
+    sys.exit()
 # Get messages from clients
 while True:
     print "Waiting for client..."
-    client, addr=UDPSock.accept()
-    print client.getpeername()," connect to server"
-    client.send("Connecting server successful".encode('utf-8'))
-    while True:
-        data = client.recv(1024)
-        print data
-        if not data:
-            client.send("The massage received".encode('utf-8'))
-            break
-    #print datetime.now().time(), " Message from:", client, ":", data.decode('utf-8')
-    client.close()
+    d, addr = UDPSock.recvfrom(1024)
+    meg, client, e, flag = readPacketsBuild(d)
+    if not flag:
+        e1 = Cal_e(meg)
+        missdata = strXor(e,e1)
+        print missdata
+        meg.insert(int(d),missdata)
+    print str(datetime.now().time())+" Message from:",client,">",buildMessage(meg)
+    UDPSock.sendto("The message received "+str(datetime.now().time()), addr)
